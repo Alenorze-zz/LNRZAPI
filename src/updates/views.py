27fcde7a5ee29b87af78e060b2fa30e444685b49
Алексей Белов <Alenorze@gmail.com>
2017-12-16 17:09:08 +0200

    JSON CBV

--- src/updates/views.py
+++ src/updates/views.py
@@ -1,5 +1,27 @@
+import json
+
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
+from django.views.generic import View
 
+from lnrzapi.mixins import JsonResponseMixin
 from .models import Update
+
+
+class JsonCBV(View):
+    def get(self, request, *args, **kwargs):
+        data = {
+            "count": 1000,
+            "content": "Some new content"
+        }
+        return JsonResponse(data)
+
+
+class JsonCBV2(View):
+    def get(self, request, *args, **kwargs):
+        data = {
+            "count": 1000,
+            "content": "Some new content"
+        }
+        return self.render_to_json_response(data)
 
