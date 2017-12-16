641e68b9669ccfecc4ca31acccc7afa81a348c46
Алексей Белов <Alenorze@gmail.com>
2017-12-16 16:59:45 +0200

    Json Response Mixin

--- src/updates/views.py
+++ src/updates/views.py
@@ -3,11 +3,3 @@ from django.shortcuts import render
 
 from .models import Update
 
-
-
-def update_user_model(request):
-    data = {
-        "count": 1000,
-        "content": "Some content"
-    }
-    return JsonResponse(data)
