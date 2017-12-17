4480243af6f692be95cbccc122c0ee9225f4b5a9
Алексей Белов <Alenorze@gmail.com>
2017-12-17 14:51:08 +0200

    DRF Support

--- src/lnrzapi/urls.py
+++ src/lnrzapi/urls.py
@@ -1,11 +1,15 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
-from updates.views import JsonCBV, JsonCBV2
-  
+from updates.views import (
+            json_example_view, 
+            JsonCBV, 
+            JsonCBV2, 
+            SerializedDetialView, 
+            SerializedListView
+    )
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^json/cbv/$', JsonCBV.as_view()),
-    url(r'^json/cbv2/$', JsonCBV2.as_view()),
+    url(r'^api/updates/', include('updates.api.urls')),
 ]
--- src/updates/apps.py
+++ src/updates/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class updatesConfig(AppConfig):
+class UpdatesConfig(AppConfig):
     name = 'updates'
