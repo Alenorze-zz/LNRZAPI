55bda7a1bc44a08312e036e77159b7e909934915
Алексей Белов <Alenorze@gmail.com>
2017-12-17 23:22:13 +0200

    Register API View

--- src/accounts/api/views.py
+++ src/accounts/api/views.py
@@ -13,7 +13,8 @@ jwt_response_payload_handler = api_settings.JWT_RESPONSE_PAYLOAD_HANDLER
 
 User = get_user_model()
 
-class AuthView(APIView):
+
+class AuthAPIView(APIView):
     permission_classes      = [permissions.AllowAny]
     def post(self, request, *args, **kwargs):
         if request.user.is_authenticated():
@@ -35,3 +36,33 @@ class AuthView(APIView):
                 response = jwt_response_payload_handler(token, user, request=request)
                 return Response(response)
         return Response({"detail": "Invalid credentials"}, status=401)
+
+
+class RegisterAPIView(APIView):
+    permission_classes      = [permissions.AllowAny]
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return Response({'detail': 'You are already registered and are authenticated.'}, status=400)
+        data = request.data
+        username        = data.get('username') # username or email address
+        email           = data.get('username')
+        password        = data.get('password')
+        password2       = data.get('password2')
+        qs = User.objects.filter(
+                Q(username__iexact=username)|
+                Q(email__iexact=username)
+            )
+        if password != password2:
+            return Response({"password": "Password must match."}, status=401)
+        if qs.exists():
+            return Response({"detail": "This user already exists"}, status=401)
+        else:
+            user = User.objects.create(username=username, email=email)
+            user.set_password(password)
+            user.save()
+            # payload = jwt_payload_handler(user)
+            # token = jwt_encode_handler(payload)
+            # response = jwt_response_payload_handler(token, user, request=request)
+            # return Response(response, status=201)
+            return Response({'detail': "Thank you for registering. Please verify your email."}, status=201)
+        return Response({"detail": "Invalid Request"}, status=400)
