6c608b6fdd062297359e0ad00afc4d28163c2bc3
Алексей Белов <Alenorze@gmail.com>
2017-12-17 18:25:46 +0200

    Status Create API View

--- src/status/api/urls.py
+++ src/status/api/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import StatusAPIView
+from .views import StatusAPIView, StatusCreateAPIView
 
 
 urlpatterns = [
     url(r'^$', StatusAPIView.as_view()),
+    url(r'^create/$', StatusCreateAPIView.as_view()),
 ]
--- src/status/api/views.py
+++ src/status/api/views.py
@@ -22,7 +22,7 @@ class StatusListSearchAPIView(APIView):
         return Response(serializer.data)
 
 
-class StatusAPiView(generics.ListAPIView):
+class StatusAPIView(generics.ListAPIView):
     permission_classes     = []
     authentication_classes = []
     serializer_class       = StatusSerializer
@@ -33,3 +33,10 @@ class StatusAPiView(generics.ListAPIView):
         if query is not None:
             qs = qs.filter(content__icontains=query)
         return qs
+
+
+class StatusCreateAPIView(generics.CreateAPIView):
+    permission_classes     = []
+    authentication_classes = []
+    queryset               = Status.objects.all()
+    serializer_class       = StatusSerializer
