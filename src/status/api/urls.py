78d9727f87243e298378b9c43ec773d56a9dafe2
Алексей Белов <Alenorze@gmail.com>
2017-12-17 17:34:35 +0200

    Status API Urls

---
+++ src/status/api/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+
+urlpatterns = [
+    url(r'^$', StatusListSearchAPIView.as_view()),
+    url(r'^create/$', StatusCreateAPIView.as_view()),
+    url(r'^(?P<id>.*)/$', StatusDetailAPIView.as_view()),
+    url(r'^(?P<id>.*)/update/$', StatusUpdateAPIView.as_view()),
+    url(r'^(?P<id>.*)/delete/$', StatusDeleteAPIView.as_view()),
+    
+]
