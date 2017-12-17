63136bca545e7da13e842a9a99c52fda7c308556
Алексей Белов <Alenorze@gmail.com>
2017-12-17 22:58:25 +0200

    JWT Authorization Header

--- src/lnrzapi/urls.py
+++ src/lnrzapi/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from rest_framework_jwt.views import refresh_jwt_token, obtain_jwt_token
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/status/', include('status.api.urls')),
+    url(r'^api/auth/jwt/$', obtain_jwt_token),
+    url(r'^api/auth/jwt/refresh/$', refresh_jwt_token),
     url(r'^api/updates/', include('updates.api.urls')), 
 ]
