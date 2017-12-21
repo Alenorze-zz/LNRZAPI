473f42db90c769ff55f3451e195f7a9f6903e04d
Алексей Белов <Alenorze@gmail.com>
2017-12-21 22:41:24 +0200

    Serializer Method Field

--- src/accounts/api/serializers.py
+++ src/accounts/api/serializers.py
@@ -1,27 +1,57 @@
 from django.contrib.auth import get_user_model
+from django.utils import timezone
+
 from rest_framework import serializers
+from rest_framework_jwt.settings import api_settings
+
+jwt_payload_handler          = api_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler           = api_settings.JWT_ENCODE_HANDLER
+jwt_response_payload_handler = api_settings.JWT_RESPONSE_PAYLOAD_HANDLER
+expire_delta                 = api_settings.JWT_REFRESH_EXPIRATION_DELTA
 
 User = get_user_model()
 
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
-    password2 = serializers.CharField(style={'input': 'password'}, write_only=True)
+    password2      = serializers.CharField(style={'input': 'password'}, write_only=True)
+    token          = serializers.SerializerMethodField(read_only=True)
+    expires        = serializers.SerializerMethodField(read_only=True)
+    token_response = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = User
         fields = [
             'username',
             'email',
             'password',
-            'password2'
+            'password2',
+            'token',
+            'expires',
+            'token_response'
         ]
         extra_kwargs = {'password': {'write_only': True}}
+    
+    def get_token_response(self, obj):
+        payload = jwt_payload_handler(user)
+        token = jwt_encode_handler(payload)
+        response = jwt_response_payload_handler(token, user, request=None)
+        return response
+
+    def get_expires(self, obj):
+        return timezone.now() + expire_delta - datetime.timedelta(seconds=200)
 
     def validate_email(self, value):
         qs = User.objects.filter(email__iexact=value)
         if qs.exists():
             raise serializers.ValidationError("User with this email already exists")
         return value
+
+    def get_token(self, obj):
+        user = obj
+        payload = jwt_payload_handler(user)
+        token = jwt_encode_handler(payload)
+        return token
 
     def validate_username(self, value):
         qs = User.objects.filter(user__iexact=value)
