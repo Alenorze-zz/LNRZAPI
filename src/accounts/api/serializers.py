08a1bd3b793320963c48f28c3d30783c14741315
Алексей Белов <Alenorze@gmail.com>
2017-12-21 23:47:42 +0200

    User API

--- src/accounts/api/serializers.py
+++ src/accounts/api/serializers.py
@@ -15,6 +15,24 @@ User = get_user_model()
 
 
 
+class UserDetailSerializer(serializers.ModelSerializer):
+    uri         = serializers.SerializerMethodField(read_only=True)
+    status_list = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'uri'
+        ]
+
+    def get_uri(self, obj):
+        return "/api/users/{id}/".format(id=obj.id)
+
+    def get_status_list(self, obj):
+        return 'obj'
+    
+
 class UserPublicSerializer(serializers.ModelSerializer):
     uri = serializers.SerializerMethodField(read_only=True)
     class Meta: 
