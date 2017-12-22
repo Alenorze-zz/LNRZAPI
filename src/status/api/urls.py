8e820bf7bdbf8d47fa20753fdf651febe28bb84d
Алексей Белов <Alenorze@gmail.com>
2017-12-22 02:45:51 +0200

    Testing Image

--- src/status/api/urls.py
+++ src/status/api/urls.py
@@ -6,7 +6,6 @@ app_name = "StatusAPI"
 
 
 urlpatterns = [
-    url(r'^$', StatusAPIView.as_view()),
-    url(r'^(?P<id>\d+)/$', StatusAPIDetailView.as_view()),
+    url(r'^$', StatusAPIView.as_view(), name='list'),
     url(r'^(?P<id>\d+)/$', StatusAPIDetailView.as_view(), name='detail'),
 ]
