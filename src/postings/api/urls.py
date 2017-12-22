235cf209cdab8d5c1215c9a08ce564f92eb09238
Алексей Белов <Alenorze@gmail.com>
2017-12-22 18:16:25 +0200

    Postings Permissions

--- src/postings/api/urls.py
+++ src/postings/api/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 
-from .views import BlogPostRUDView
+from rest_framework_jwt.views import obtain_jwt_token
 
+from .views import BlogPostRUDView
 
 app_name = 'RUDPost'
 
 
 urlpatterns = [
-    url(r'^(?P<pk>\d+)/$', BlogPostRUDView.as_view(), name='post-rud')
+    url(r'^(?P<pk>\d+)/$', BlogPostRUDView.as_view(), name='post-rud'),
+    url(r'^api/auth/login/$', obtain_jwt_token, name='api-login'),
 ]
